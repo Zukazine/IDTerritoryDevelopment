4790222bdd25ac454e140583771425ae41f90824
Zukazine <zukazine007@gmail.com>
2023-11-30 09:30:14 +0700

    update SDA section

--- pages/1_SDA.py
+++ pages/1_SDA.py
@@ -11,6 +11,7 @@ st.title('SDA Dashboard 🌊')
 
 left, right = st.columns(2)
 
+
 with left:
     province = st.selectbox('Pilih Provinsi', ['Sulawesi Selatan', 'Lampung', 'Jawa Tengah', 'Bali', 'Aceh','Sumatera Utara', 'Sumatera Selatan', 'Banten', 'Nusa Tenggara Barat', 'Kalimantan Selatan', 'Jawa Timur', 'Daerah Istimewa Yogyakarta', 'Sumatera Barat', 'Jawa Barat', 'Sulawesi Utara', 'Bangka Belitung', 'Bengkulu', 'Gorontalo', 'Jambi', 'Kalimantan Tengah', 'Kepulauan Riau', 'Maluku', 'Maluku Utara', 'Nusa Tenggara Timur', 'Riau', 'Papua Barat', 'Sulawesi Barat', 'Sulawesi Tengah'])
 
@@ -48,9 +49,229 @@ def marker():
 if province and sector:
     with st.spinner('Constructing ...'):
         folium_static(marker(), width=850, height=400)
+    
+    with st.expander(":red[**Parameter Prioritasisasi**]"):
+        params = st.selectbox('Pilih Parameter', ['Kemiringan Lereng', 'Jenis Tutupan Lahan (LULC)', 'Presipitasi','Populasi', 'Normalized Difference Salinity Index', 'Jaringan Jalan', 'Jaringan Sungai', 'Soil pH', 'Soil Clay', 'Infrastruktur Air', 'Produktivitas Pertanian Padi', 'Neraca Air'])
+        if params:
+            if params == 'Kemiringan Lereng':
+                
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Kemiringan lereng menjadi faktor yang akan memengaruhi persiapan lahan yang dapat diairi, operasi irigasi, efisiensi irigasi efisiensi, erosi, biaya penyiapan lahan, jenis tanaman, biaya produksi produksi, dan metode irigasi (Hussien dkk., 2019).''')
+                st.image('images/1_Slope.png')
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                NASA SRTM Digital Elevation 30m
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-30m
+                                ''')
+            if params == 'Jenis Tutupan Lahan (LULC)':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Jenis tutupan lahan menjadi parameter penting  dalam analisis kesesuaian lahan untuk irigasi permukaan (Hagos dkk., 2022). Hal ini karena LULC membantu mengidentifikasi produktivitas suatu wilayah untuk irigasi.''')
+                st.image('images/2_LULC.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                ESA WorldCover 10m v100
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-100m
+                                ''')
+            if params == 'Presipitasi':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Curah hujan sangat penting dalam penilaian irigasi permukaan untuk memastikan bahwa ada cukup air yang tersedia untuk irigasi dan untuk menentukan kesesuaian metode irigasi (Hagos dkk., 2022).''')
+                
+                st.image('images/3_Precipitation.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                TerraClimate: Monthly Climate and Climatic Water Balance for Global Terrestrial Surfaces, University of Idaho
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-4800m
+                                ''')
+            if params == 'Populasi':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Mempertimbangkan distribusi kepadatan penduduk merupakan hal yang penting dalam penilaian irigasi permukaan untuk memahami permintaan produk pertanian dan ketersediaan tenaga kerja, yang merupakan faktor penting dalam menentukan kesesuaian suatu daerah untuk irigasi (Worqlul dkk., 2018).''')
 
-    with st.expander("**Penjelasan Parameter Prioritasisasi**"):
-        st.markdown('Berikut merupakan parameter yang digunakan sehingga peta prioritas bisa terbentuk : ')
+                st.image('images/4_Population.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                WorldPop Global Project Population Data: Estimated Residential Population per 100x100m Grid Square
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-100m
+                                ''')
+            if params == 'Normalized Difference Salinity Index':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            NDSI dapat membantu mengidentifikasi area yang terkena dampak salinitas di dalam ladang irigasi, memungkinkan strategi irigasi yang ditargetkan dan praktik pengelolaan tanah untuk mengurangi dampak salinitas pada produktivitas pertanian (Wubalem, 2023).''')
+
+                st.image('images/5_NDSI.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                USGS Landsat 8 Collection 2 Tier 1 TOA Reflectance (Band 3 dan Band 4)
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-30m
+                                ''')
+            if params == 'Jaringan Jalan':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Kedekatan jalan raya dipertimbangkan dalam analisis kesesuaian daerah irigasi permukaan karena pengaruhnya terhadap akses ke input pertanian, outlet pasar, dan pusat-pusat kota, serta dampaknya terhadap operasi dan efisiensi irigasi (Kassie dkk., 2022).''')
+
+                st.image('images/6_RoadNetwork.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                GRIP global roads database
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Vektor-polyline
+                                ''')
+            if params == 'Jaringan Sungai':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Jarak antara lahan yang dapat diairi dan sungai merupakan faktor penting dalam menentukan kesesuaian suatu daerah untuk irigasi permukaan, dan daerah yang terletak dalam jarak tertentu dari sungai dianggap sangat cocok untuk irigasi (Getahun dkk., 2023).''')
+
+                st.image('images/7_RiverNetwork.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                Global River Classification (GloRiC)
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Vektor-polyline
+                                ''')
+            if params == 'Soil pH':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Tingkat keasaman tanah adalah salah satu faktor yang perlu diperiksa untuk kesesuaian irigasi permukaan karena mempengaruhi hasil panen (USDIBR, 2003).''')
+
+                st.image('images/8_pH.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                Soil Grids 250m v2.0
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-250m
+                                ''')
+            if params == 'Soil Clay':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Pertimbangan tanah lempung penting dalam menganalisis kesesuaian daerah irigasi permukaan karena tanah lempung menyimpan lebih banyak air daripada tanah berpasir maka irigasi permukaan dapat bekerja lebih baik pada kondisi tanah liat atau pasir yang berat dibandingkan dengan alat penyiram. Namun, penting untuk dicatat bahwa tanah lempung yang berat umumnya harus dihindari untuk irigasi, karena sulit untuk dikelola dan dapat menyebabkan limpasan air daripada penyerapan (Department for Environment and Water, 2023).''')
+
+                st.image('images/9_Clay.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                Soil Grids 250m v2.0
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Raster-250m
+                                ''')
+            if params == 'Infrastruktur Air':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            - **Bendungan operasi** memainkan peran penting dalam penyimpanan dan distribusi air untuk tujuan irigasi karena akan memberikan wawasan tentang ketersediaan, regulasi, dan pengelolaan sumber daya air untuk irigasi permukaan (FAO, 1989).  
+                            - Data **Intake Sungai** dapat membantu mengidentifikasi kesesuaian infrastruktur irigasi yang ada atau yang diusulkan, seperti kanal, waduk, dan stasiun pompa (FAO, 1989). Informasi ini sangat penting untuk merencanakan dan melaksanakan pembangunan irigasi berkelanjutan dan memastikan distribusi air yang efisien ke daerah irigasi.  
+                            - Sedangkan untuk data **Daerah Irigasi Permukaan** merupakan data yang digunakan untuk memperlihatkan daerah irigasi di lahan sawah nontadah hujan. 
+                            ''')
+                st.markdown('')
+                st.image('images/10_SIA_RI_D.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                SIGI PUPR
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Vektor-point
+                                ''')
+            if params == 'Produktivitas Pertanian Padi':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Data produksi padi dapat digunakan untuk penilaian irigasi permukaan untuk memahami dampak praktik irigasi terhadap budidaya padi dan untuk mengidentifikasi area di mana pengelolaan air dan strategi irigasi dapat meningkatkan produktivitas padi. ''')
+
+                st.image('images/11_Rice.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                BDSP Kementan
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Tabular
+                                ''')
+            if params == 'Neraca Air':
+                st.markdown('''
+                            :red[**Penjelasan**]  
+                            Penggunaan data neraca air dalam penilaian irigasi permukaan berperan dalam memahami ketersediaan air, efisiensi irigasi, pengelolaan air, dampak variabilitas iklim, dan optimalisasi praktik irigasi.
+                            ''')
+
+                st.image('images/12_wb.png')
+                
+                right, left = st.columns(2)
+                with right: 
+                    st.markdown('''
+                                :red[**Sumber Data**]  
+                                SIGI PUPR
+                                ''')
+                with left:
+                    st.markdown('''
+                                :red[**Tipe Data dan Resolusi**]  
+                                Vektor-polygon
+                                ''')
+
 
     with st.expander("**Ide Pengembangan**"):
         st.markdown('''
